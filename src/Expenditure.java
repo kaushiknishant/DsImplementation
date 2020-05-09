f433ac3b55e66aaa4ed0846a3d551923ff0d98fd
nishantsharma12 <nishant@navigus.in>
2020-05-09 23:57:12 +0530

    competitive program

---
+++ src/Expenditure.java
@@ -0,0 +1,58 @@
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.regex.*;
+
+public class Expenditure {
+
+    // Complete the activityNotifications function below.
+    static int activityNotifications(int[] expenditure, int d) {
+        int 
+        int mid = expenditure.length/2;
+        int med;
+        if(expenditure.length % 2 == 0){
+            int mid1 = mid+1;
+            med = expenditure[mid] + expenditure[mid1]/2;
+        }
+        else{
+            med = expenditure[mid]/2;
+        }
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] expenditure = new int[n];
+
+        String[] expenditureItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int expenditureItem = Integer.parseInt(expenditureItems[i]);
+            expenditure[i] = expenditureItem;
+        }
+
+        int result = activityNotifications(expenditure, d);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
